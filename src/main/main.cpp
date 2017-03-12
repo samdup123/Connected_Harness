d467cfb94a57846c2e44eb9efb268e9df789c97c
Sam <samdup123@gmail.com>
2017-03-11 23:14:58 -0500

    update gitignore

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,23 +1,31 @@
 extern "C"
 {
 #include <uno_GpioGroup.h>
-#include "DigitalOutput_GpioGroupAdapter.h"
+#include "DigitalInput_GpioGroupAdapter.h"
+#include "DigitalInputPullup_GpioGroupAdapter.h"
 #include "utilities.h"
 }
 #include "Arduino.h"
 
 static ty_i_GpioGroup *gpio;
-static ty_DigitalOutput_GpioGroupAdapter digOut;
+static ty_DigitalInput_GpioGroupAdapter digIn;
+static ty_DigitalInputPullup_GpioGroupAdapter digInPullup;
 
 void setup() {
-//	gpio = init_uno_GpioGroup();
-//	Serial.begin(9600);
-//
-//	init_DigitalInputPullup_GpioGroupAdapter(&digitalInputPullupAdapter, gpio, 4);
+	gpio = init_uno_GpioGroup();
+	Serial.begin(9600);
+
+	init_DigitalInputPullup_GpioGroupAdapter(&digInPullup, gpio, 4);
+	init_DigitalInput_GpioGroupAdapter(&digIn, gpio, 4);
 }
 
 void loop() {
-//	bool state = Read_DigitalInputPullup(&digitalInputPullupAdapter.interface);
-//	Write_GpioGroup(gpio, 2, state);
-//	delay(1);
+	bool state = Read_DigitalInputPullup(&digInPullup.interface);
+	Serial.print("\npullup");
+	Serial.println(state);
+
+    state = Read_DigitalInputPullup(&digIn.interface);
+	Serial.print("\nnotpullup");
+	Serial.println(state);
+	delay(700);
 }
